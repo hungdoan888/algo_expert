d6fd946422d99efe357dfa48c004d5957c38659a
hungdoan888 <73352631+hungdoan888@users.noreply.github.com>
2020-12-19 15:29:20 -0800

    boggleBoard update

--- boggleBoard.py
+++ boggleBoard.py
@@ -13,16 +13,11 @@ class SuffixTrie:
         self.endSymbol = "*"
 
     def populateSuffixTrieFrom(self, string):
-        # Write your code here.
-        for i in range(len(string)):
-            self.populateSuffixTrieFromHelper(string, i)
-        
-    def populateSuffixTrieFromHelper(self, string, i):
         node = self.root
-        for j in range(i, len(string)):
-            if string[j] not in node:
-                node[string[j]] = {}
-            node = node[string[j]]
+        for i in range(len(string)):
+            if string[i] not in node:
+                node[string[i]] = {}
+            node = node[string[i]]
         node[self.endSymbol] = True
 
     def contains(self, string):
@@ -46,7 +41,58 @@ def boggleBoard(board, words):
     for i in range(len(words)):
         trie.populateSuffixTrieFrom(words[i])
         
+    # Traverse Board
+    wordsInBoard = {}
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            node = trie.root
+            visited = [[False for l in range(len(board[0]))] for k in range(len(board))]
+            word = ""
+            wordsInBoard = boggleBoardHelper(board, node, visited, word, wordsInBoard, i, j)
+    return list(wordsInBoard.keys())
 
+def boggleBoardHelper(board, node, visited, word, wordsInBoard, i, j):
+    
+    if board[i][j] not in node:
+        return wordsInBoard
+    
+    node = node[board[i][j]]            
+    visited[i][j] = True
+    word = word + board[i][j]
+
+    if "*" in node:
+        if word not in wordsInBoard:
+            wordsInBoard[word] = True
+    
+    neighbors = neighborsList(visited, board, i, j)
+    for k in range(len(neighbors)):
+        wordsInBoard = boggleBoardHelper(board, 
+                                         node, 
+                                         visited, 
+                                         word, 
+                                         wordsInBoard, 
+                                         neighbors[k][0], 
+                                         neighbors[k][1])
+        
+    visited[i][j] = False
+    return wordsInBoard
+        
+        
+def neighborsList(visited, board, i, j):
+    
+    neighbors = []
+    for k in range(i-1, i+2):
+        for l in range(j-1, j+2):
+            if k < 0 or l < 0:
+                continue
+            if k >= len(board) or l >= len(board[0]):
+                continue
+            if k == i and l == j:
+                continue
+            if visited[k][l] is True:
+                continue
+            neighbors.append([k,l])
+    return neighbors
 
 # Input
 board = [
@@ -74,4 +120,5 @@ words = [
   "NOTRE-PEATED"
 ]
 
+boggleBoard(board, words)
 
