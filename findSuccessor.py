06fa75f13116276705bef57fbf6d26654ecfc1f6
hungdoan888 <73352631+hungdoan888@users.noreply.github.com>
2020-12-26 12:23:19 -0800

    Create findSuccessor.py

---
+++ findSuccessor.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 26 11:48:46 2020
+
+@author: hungd
+"""
+
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+def findSuccessor(tree, node):
+    # Write your code here.
+    return None
