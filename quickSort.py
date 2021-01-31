b8db337d55fb50da4c8578eda0cd15298a9def19
hungdoan888 <73352631+hungdoan888@users.noreply.github.com>
2021-01-30 21:47:31 -0800

    Update quickSort.py

--- quickSort.py
+++ quickSort.py
@@ -7,14 +7,11 @@ Created on Sat Jan 30 17:35:55 2021
 
 def quickSort(array):
     # Write your code here.
-    quickSortHelper(array, p = 0, l = 1, r = len(array) - 1)
+    return quickSortHelper(array, p = 0, l = 1, r = len(array) - 1)
     
-            
-
 def quickSortHelper(array, p, l, r):
     # Write your code here.
-    print(array)
-    rEnd = r
+    
     while r >= l:
         if array[l] > array[p] and array[r] < array[p]:
             swap(array, l, r)
@@ -28,15 +25,15 @@ def quickSortHelper(array, p, l, r):
     swap(array, p, r)
     
     if len(array[:r]) > 1:
-        quickSortHelper(array, p, p + 1, r - 1)
+        array[:r] = quickSortHelper(array[:r], 0, 1, len(array[:r]) - 1)
         
     if r + 1 < len(array) and len(array[r + 1:]) > 1:
-        quickSortHelper(array, r + 1, r + 2, rEnd)
-        
+        array[r + 1:] = quickSortHelper(array[r + 1:], 0, 1, len(array[r + 1:]) - 1)
+    
     return array
             
 def swap(array, l, r):
     array[l], array[r] = array[r], array[l]
 
 array = [8, 5, 2, 9, 5, 6, 3]
-quickSortHelper(array, p = 0, l = 1, r = len(array) - 1)
+quickSortHelper(array, 0, 1, len(array) - 1)
