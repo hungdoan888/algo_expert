9167fd58d5f767081492ae5a0458819ee1bebd9d
hungdoan888 <73352631+hungdoan888@users.noreply.github.com>
2021-02-28 20:46:50 -0800

    Update kthLargestValueInBST.py

--- kthLargestValueInBST.py
+++ kthLargestValueInBST.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 21 21:31:29 2021
-
-@author: hungd
-"""
-
 # This is an input class. Do not edit.
 class BST:
     def __init__(self, value, left=None, right=None):
@@ -12,48 +5,47 @@ class BST:
         self.left = left
         self.right = right
         
-class kthLargestNodeValue:
+class kthLargestValueClass:
     def __init__(self):
         self.value = None
 
+
 def findKthLargestValueInBst(tree, k):
     # Write your code here.
-    kthLargestValue = kthLargestNodeValue()
-    findKthLargestValueInBstHelper(tree, k, kthLargestValue)
+    l = 0
+    kthLargestValue = kthLargestValueClass()
+    findKthLargestValueInBstHelper(tree, k, l, kthLargestValue)
     return kthLargestValue.value
-    
-def findKthLargestValueInBstHelper(node, k, kthLargestValue):
-    
+
+def findKthLargestValueInBstHelper(node, k, l, kthLargestValue):
+    # Write your code here.
     if node is None:
-        return 1
-    
-    kthLargestNodeIndex = findKthLargestValueInBstHelper(node.right, k, kthLargestValue)
+        return l
     
     if kthLargestValue.value is not None:
-        return
+        return l
     
-    if kthLargestNodeIndex == k:
+    # right
+    l = findKthLargestValueInBstHelper(node.right, k, l, kthLargestValue)
+    
+    # visit
+    l += 1
+    if l == k:
         kthLargestValue.value = node.value
-        
-    elif kthLargestNodeIndex == k - 1 and node.left is not None:
-        kthLargestValue.value = getMaxValue(node.left)
-        
-    else:   
-        return kthLargestNodeIndex + 1
+        return l
     
-def getMaxValue(node):
-    if node.right is None:
-        return node.value
-    return getMaxValue(node.right)
-        
-tree = BST(20)
-tree.left = BST(15)
-tree.right = BST(25)
-tree.left.left = BST(10)
-tree.left.right = BST(19)
-tree.right.left = BST(21)
-tree.right.right = BST(30)
-tree.right.left.right = BST(22)
-
+    # left
+    return findKthLargestValueInBstHelper(node.left, k, l, kthLargestValue)
+    
+    
+tree = BST(15)
+tree.left = BST(5)
+tree.left.left = BST(2)
+tree.left.left.left = BST(1)
+tree.left.left.right = BST(3)
+tree.left.right = BST(5)
+tree.right = BST(20)
+tree.right.left = BST(17)
+tree.right.right = BST(22)
 k = 3
 findKthLargestValueInBst(tree, k)
