01f2e3914b9c40d46c770423bc42c212d2aa745d
hungdoan888 <73352631+hungdoan888@users.noreply.github.com>
2021-04-27 22:28:35 -0700

    algoExpert

--- overlappingIntervals.py
+++ overlappingIntervals.py
@@ -5,3 +5,23 @@ Created on Sun Apr 25 21:28:56 2021
 @author: hungd
 """
 
+def mergeOverlappingIntervals(intervals):
+    # Write your code here.
+    
+    intervals = sorted(intervals, key=lambda x: x[0])
+    overlappingIntervals = [intervals[0]]
+    for i in range(1, len(intervals)):
+    	if intervals[i][0] <= overlappingIntervals[-1][1]:
+    		overlappingIntervals[-1][1] = max(intervals[i][1], overlappingIntervals[-1][1])
+    	else:
+    		overlappingIntervals.append(intervals[i])
+    return overlappingIntervals
+
+intervals = [
+    [2, 3],
+    [4, 5],
+    [6, 7],
+    [8, 9],
+    [1, 10]
+  ]
+mergeOverlappingIntervals(intervals)
--- tandemBicycle.py
+++ tandemBicycle.py
@@ -5,3 +5,43 @@ Created on Sun Apr 25 18:21:04 2021
 @author: hungd
 """
 
+def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
+    # Write your code here.
+    redShirtSpeeds.sort()
+    blueShirtSpeeds.sort()
+    if fastest:
+        return tandemFastest(redShirtSpeeds, blueShirtSpeeds)
+    else:
+        return tandemSlowest(redShirtSpeeds, blueShirtSpeeds)
+
+def tandemFastest(red, blue):
+	redLeft = 0
+	redRight = len(red) - 1
+	blueLeft = 0
+	blueRight = len(blue) - 1
+	
+	tandemSum = 0
+	while redLeft <= redRight and blueLeft <= blueRight:
+		if red[redRight] >= blue[blueRight]:
+			tandemSum += red[redRight]
+			redRight -= 1
+			blueLeft += 1
+		else:
+			tandemSum += blue[blueRight]
+			redLeft += 1
+			blueRight -= 1
+	return tandemSum
+
+def tandemSlowest(red, blue):
+	tandemSum = 0
+	for i in range(len(red)):
+		if red[i] >= blue[i]:
+			tandemSum += red[i]
+		else:
+			tandemSum += blue[i]
+	return tandemSum
+
+redShirtSpeeds = [5, 5, 3, 9, 2]
+blueShirtSpeeds = [3, 6, 7, 2, 1]
+fastest = False
+tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest)
